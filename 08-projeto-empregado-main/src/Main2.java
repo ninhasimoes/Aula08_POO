aa06fda9127ed15698b1f9a350343ca3f19ba8ea
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-28 09:27:03 -0300

    Exercício 1

---
+++ 08-projeto-empregado-main/src/Main2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //Quando n sabe a quant de dados fazemos um vetor dinamico
+        //array redimensionavel
+        ArrayList <Empregado> lista = new ArrayList<>();
+        //com arraylist só se usa metodos, pois ele é um objeto
+        //instanciando um objeto
+        //overload - sobrecarga de metodos
+        lista.add(new EmpregadoComissionado(1,"A",10,10));
+        lista.add(new EmpregadoHorista(2,"B",10,10));
+
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i).nome);
+            System.out.println(lista.get(i).matricula);
+            System.out.println();
+        }
+
+        //funciona mais para array dinamica
+        //for genérico
+        for (Empregado e : lista){
+            System.out.println(e);
+            System.out.println(e.calcularSalario());
+        }
+
+    }
+}
